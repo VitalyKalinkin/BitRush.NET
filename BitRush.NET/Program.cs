11e80366cc344be2ac7945b3b3ee91059af0d55d
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-04-07 08:43:40 +0400

    Initial project stucture

---
+++ BitRush.NET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace BitRush.NET
+{
+    class Program
+    {
+        private static ILog Logger = LogManager.GetLogger(typeof(Program));
+        
+        static void Main(string[] args)
+        {
+            Logger.InfoFormat("Starting application with args: {0}", args);
+        }
+    }
+}
